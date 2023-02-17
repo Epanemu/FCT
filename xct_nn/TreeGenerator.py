1c455e5f1a944d6e53e889ead8b13662a32623e5
Jiri Nemecek <contact@nemecekjiri.cz>
2023-02-17 15:54:37 +0100

    Add sklearn support

--- xct_nn/TreeGenerator.py
+++ xct_nn/TreeGenerator.py
@@ -8,7 +8,8 @@ class TreeGenerator:
 
     def make_from_context(self, context):
         decision_features = context["a"].argmax(axis=0)
-        thresholds = context["b"].round(self.data_h.round_limit) * self.data_h.scales[decision_features] + self.data_h.shifts[decision_features]
+        # thresholds = context["b"].round(self.data_h.round_limit) * self.data_h.scales[decision_features] + self.data_h.shifts[decision_features]
+        thresholds = self.__thresholds_from_b(context["b"], decision_features)
         leaf_assignments = context["classes"].argmax(axis=0)
 
         return ClassificationTree(context, decision_features, thresholds, leaf_assignments)
@@ -18,7 +19,8 @@ class TreeGenerator:
             depth = self.__depth_from_branchnodes(b_matrix.shape[0])
 
         decision_features = a_matrix.argmax(axis=0)
-        thresholds = b_matrix.round(self.data_h.round_limit) * self.data_h.scales[decision_features] + self.data_h.shifts[decision_features]
+        # thresholds = b_matrix.round(self.data_h.round_limit) * self.data_h.scales[decision_features] + self.data_h.shifts[decision_features]
+        thresholds = self.__thresholds_from_b(b_matrix, decision_features)
         leaf_assignments = classes_matrix.argmax(axis=0)
 
         base_context = {
@@ -41,3 +43,50 @@ class TreeGenerator:
 
     def __depth_from_branchnodes(self, branch_nodes):
         return np.log2(branch_nodes + 1).astype(int)
+
+    def __thresholds_from_b(self, b, dec_features):
+        return np.clip(b, 0, 1).round(self.data_h.round_limit) * self.data_h.scales[dec_features] + self.data_h.shifts[dec_features]
+
+    def __depth_from_index(self, i):
+        return np.floor(np.log2(i + 1)).astype(int)
+
+    def make_from_sklearn(self, sklearn_tree, train_data):
+        totdepth = sklearn_tree.max_depth
+        decision_features = np.zeros((2**totdepth - 1,), dtype=int)
+        thresholds = np.zeros((2**totdepth - 1,))
+        leaf_assignments = np.zeros((2**totdepth,), dtype=int)
+        map_i = np.full((len(sklearn_tree.children_left),), -1)
+        queue = [(0, 0, 0)]
+        while queue:
+            i, depth, mapped = queue.pop(0)
+            map_i[i] = mapped
+            if sklearn_tree.children_left[i] != -1: # if node is not a leaf
+                queue.append((sklearn_tree.children_left[i], depth+1, mapped*2+1))
+                queue.append((sklearn_tree.children_right[i], depth+1, mapped*2+2))
+                decision_features[mapped] = sklearn_tree.feature[i]
+
+                # sklearn uses <=, this implementation uses <
+                vals = train_data[:, sklearn_tree.feature[i]]
+                orig_thresh = sklearn_tree.threshold[i]
+                if (vals == orig_thresh).any():
+                    thresholds[mapped] = np.min(vals[vals > orig_thresh])
+                else:
+                    thresholds[mapped] = orig_thresh
+            else:
+                depth_remaining = totdepth - self.__depth_from_index(mapped)
+                for _ in range(depth_remaining):
+                    mapped = mapped*2 + 2
+                leaf_assignments[mapped - 2**totdepth + 1] = np.argmax(sklearn_tree.value[i])
+
+        a_matrix = np.zeros((self.data_h.n_features, decision_features.shape[0]), dtype=int)
+        a_matrix[decision_features, np.arange(decision_features.shape[0])] = 1
+        classes_matrix = np.zeros((self.data_h.n_classes, leaf_assignments.shape[0]), dtype=int)
+        classes_matrix[leaf_assignments, np.arange(leaf_assignments.shape[0])] = 1
+        base_context = {
+            "data_h": self.data_h,
+            "depth": totdepth,
+            "a": a_matrix,
+            "b": thresholds,
+            "classes": classes_matrix,
+        }
+        return ClassificationTree(base_context, decision_features, self.__thresholds_from_b(thresholds, decision_features), leaf_assignments)
