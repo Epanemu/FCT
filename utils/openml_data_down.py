8a7e430e73a1130c7a578740239386db4cfd4492
Jiri Nemecek <contact@nemecekjiri.cz>
2023-05-23 20:23:11 +0200

    Uncomment the download script

--- utils/openml_data_down.py
+++ utils/openml_data_down.py
@@ -1,41 +1,39 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-import time
 import os
-import sys
 import openml
 openml.config.cache_directory = os.path.expanduser(os.getcwd() + "/openml_cache")
 import pickle
 
-suites_id = {"numerical_regression": 297,
-          "numerical_classification": 298,
-          "categorical_regression": 299,
-          "categorical_classification": 304}
+suites_id = {"numerical_classification": 337,
+          "categorical_classification": 334}
 
-# benchmark_suite = openml.study.get_suite(suites_id["numerical_classification"])  # obtain the benchmark suite
-# for k, task_id in enumerate(benchmark_suite.tasks):  # iterate over all tasks
-#     task = openml.tasks.get_task(task_id)  # download the OpenML task
-#     dataset = task.get_dataset()
-#     print(f"Handling dataset {dataset.name} ({k+1}/{len(benchmark_suite.tasks)})")
-#     X, y, categorical_indicator, attribute_names = dataset.get_data(
-#         dataset_format="dataframe", target=dataset.default_target_attribute
-#     )
+os.makedirs("data/openml/numerical", exist_ok=True)
+os.makedirs("data/openml/categorical", exist_ok=True)
 
-#     with open(f"data/openml/numerical/{dataset.name}.pickle", "wb") as f:
-#         pickle.dump((X, y, categorical_indicator, attribute_names, dataset.name), f)
+benchmark_suite = openml.study.get_suite(suites_id["numerical_classification"])  # obtain the benchmark suite
+for k, task_id in enumerate(benchmark_suite.tasks):  # iterate over all tasks
+    task = openml.tasks.get_task(task_id)  # download the OpenML task
+    dataset = task.get_dataset()
+    print(f"Handling dataset {dataset.name} ({k+1}/{len(benchmark_suite.tasks)})")
+    X, y, categorical_indicator, attribute_names = dataset.get_data(
+        dataset_format="dataframe", target=dataset.default_target_attribute
+    )
 
-# benchmark_suite = openml.study.get_suite(suites_id["categorical_classification"])  # obtain the benchmark suite
-# for k, task_id in enumerate(benchmark_suite.tasks):  # iterate over all tasks
-#     task = openml.tasks.get_task(task_id)  # download the OpenML task
-#     dataset = task.get_dataset()
-#     print(f"Handling dataset {dataset.name} ({k+1}/{len(benchmark_suite.tasks)})")
-#     X, y, categorical_indicator, attribute_names = dataset.get_data(
-#         dataset_format="dataframe", target=dataset.default_target_attribute
-#     )
+    with open(f"data/openml/numerical/{dataset.name}.pickle", "wb") as f:
+        pickle.dump((X, y, categorical_indicator, attribute_names, dataset.name), f)
 
-#     with open(f"data/openml/categorical/{dataset.name}.pickle", "wb") as f:
-#         pickle.dump((X, y, categorical_indicator, attribute_names, dataset.name), f)
+benchmark_suite = openml.study.get_suite(suites_id["categorical_classification"])  # obtain the benchmark suite
+for k, task_id in enumerate(benchmark_suite.tasks):  # iterate over all tasks
+    task = openml.tasks.get_task(task_id)  # download the OpenML task
+    dataset = task.get_dataset()
+    print(f"Handling dataset {dataset.name} ({k+1}/{len(benchmark_suite.tasks)})")
+    X, y, categorical_indicator, attribute_names = dataset.get_data(
+        dataset_format="dataframe", target=dataset.default_target_attribute
+    )
+
+    with open(f"data/openml/categorical/{dataset.name}.pickle", "wb") as f:
+        pickle.dump((X, y, categorical_indicator, attribute_names, dataset.name), f)
 
 directory = f"data/openml/categorical/"
 for filename in os.listdir(directory):
